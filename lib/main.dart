a7943308ebf18fe22fece43166047ea0f63c2857
ahmedyar7 <ahmedyar778@gmail.com>
2025-05-14 15:51:07 +0500

    rarranging the paths

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env"); // LOAD THE KEY
   runApp(
     MultiProvider(
       providers: [
